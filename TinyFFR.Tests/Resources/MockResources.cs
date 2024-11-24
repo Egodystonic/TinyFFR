4ea7a72f6d12b6a79bf65e5b3a3035759908b7a6
Ben Bowen <Xenoprimate@users.noreply.github.com>
2024-11-24 18:35:56 +0000

    WIP, pushing to work on laptop

--- TinyFFR.Tests/Resources/MockResources.cs
+++ TinyFFR.Tests/Resources/MockResources.cs
@@ -5,18 +5,24 @@ namespace Egodystonic.TinyFFR.Resources;
 
 interface IMockResourceImplProvider : IResourceImplProvider;
 
-readonly struct MockHandleAlpha : IResourceHandle<MockHandleAlpha> {
-	public Func<MockHandleAlpha, bool>? OnEquals { get; init; }
-
+readonly unsafe struct MockHandleAlpha : IResourceHandle<MockHandleAlpha> {
 	public UIntPtr AsInteger { get; init; }
-	public unsafe void* AsPointer { get; init; }
+	public void* AsPointer { get; init; }
 	public ResourceIdent Ident { get; init; }
 	public static IntPtr TypeHandle => typeof(MockResourceAlpha).TypeHandle.Value;
 
-	public bool Equals(MockHandleAlpha other) 
-	public static MockHandleAlpha CreateFromInteger(UIntPtr integer) { return default; }
-	public static unsafe MockHandleAlpha CreateFromPointer(void* pointer) { return default; }
+	public bool Equals(MockHandleAlpha other) => AsInteger.Equals(other.AsInteger) && AsPointer == other.AsPointer;
+	public static MockHandleAlpha CreateFromInteger(UIntPtr integer) => new() { AsInteger = integer, AsPointer = (void*) integer, Ident = new(TypeHandle, integer) };
+	public static MockHandleAlpha CreateFromPointer(void* pointer) => new() { AsInteger = (UIntPtr) pointer, AsPointer = pointer, Ident = new(TypeHandle, (UIntPtr) pointer) };
 } 
 readonly struct MockResourceAlpha : IResource<MockResourceAlpha, MockHandleAlpha, IMockResourceImplProvider> {
-
+	readonly string? _name;
+	public ReadOnlySpan<char> Name {
+		get => _name;
+		init => _name = value.ToString();
+	}
+	public bool Equals(MockResourceAlpha other) { throw new NotImplementedException(); }
+	public static MockResourceAlpha RecreateFromRawHandleAndImpl(UIntPtr rawHandle, IResourceImplProvider impl) { throw new NotImplementedException(); }
+	public MockHandleAlpha Handle { get; }
+	public IMockResourceImplProvider Implementation { get; }
 }
