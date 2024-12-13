3ccf777b6077bc6de86fdb30089a69adb16c28ab
Ben Bowen <Xenoprimate@users.noreply.github.com>
2024-12-13 15:51:47 +0000

    WIP addition of lighting

---
+++ TinyFFR/World/Lighting/ILightBuilder.cs
@@ -0,0 +1,34 @@
+﻿// Created on 2024-08-07 by Ben Bowen
+// (c) Egodystonic / TinyFFR 2024
+
+using System;
+using Egodystonic.TinyFFR.Assets.Materials;
+using Egodystonic.TinyFFR.Assets.Meshes;
+
+namespace Egodystonic.TinyFFR.World;
+
+public interface ILightBuilder {
+	ModelInstance CreateModelInstance(Mesh mesh, Material material, Location? initialPosition = null, Rotation? initialRotation = null, Vect? initialScaling = null, ReadOnlySpan<char> name = default) {
+		return CreateModelInstance(
+			mesh,
+			material,
+			new Transform(
+				translation: initialPosition?.AsVect() ?? ModelInstanceCreationConfig.DefaultInitialTransform.Translation,
+				rotation: initialRotation ?? ModelInstanceCreationConfig.DefaultInitialTransform.Rotation,
+				scaling: initialScaling ?? ModelInstanceCreationConfig.DefaultInitialTransform.Scaling
+			),
+			name
+		);
+	}
+	ModelInstance CreateModelInstance(Mesh mesh, Material material, Transform initialTransform, ReadOnlySpan<char> name = default) {
+		return CreateModelInstance(
+			mesh,
+			material,
+			new ModelInstanceCreationConfig {
+				InitialTransform = initialTransform,
+				Name = name
+			}
+		);
+	}
+	ModelInstance CreateModelInstance(Mesh mesh, Material material, in ModelInstanceCreationConfig config);
+}
---
+++ TinyFFR/World/Lighting/Light.cs
@@ -0,0 +1,77 @@
+﻿// Created on 2024-08-07 by Ben Bowen
+// (c) Egodystonic / TinyFFR 2024
+
+using System;
+using Egodystonic.TinyFFR.Assets.Materials;
+using Egodystonic.TinyFFR.Assets.Meshes;
+using Egodystonic.TinyFFR.Resources;
+
+namespace Egodystonic.TinyFFR.World;
+
+public readonly struct Light : IDisposableResource<Light, LightHandle, ILightImplProvider>, IPositionedSceneObject, IOrientedSceneObject {
+	readonly LightHandle _handle;
+	readonly ILightImplProvider _impl;
+
+	internal ILightImplProvider Implementation => _impl ?? throw InvalidObjectException.InvalidDefault<Light>();
+	internal LightHandle Handle => IsDisposed ? throw new ObjectDisposedException(nameof(Light)) : _handle;
+
+	ILightImplProvider IResource<LightHandle, ILightImplProvider>.Implementation => Implementation;
+	LightHandle IResource<LightHandle, ILightImplProvider>.Handle => Handle;
+
+	public ReadOnlySpan<char> Name {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => Implementation.GetName(_handle);
+	}
+
+	public Location Position {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => Implementation.GetPosition(_handle);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		set => Implementation.SetPosition(_handle, value);
+	}
+	public Rotation Rotation {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => Implementation.GetRotation(_handle);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		set => Implementation.SetRotation(_handle, value);
+	}
+
+	public ColorVect Color {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => Implementation.GetColor(_handle);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		set => Implementation.SetColor(_handle, value);
+	}
+
+	internal Light(LightHandle handle, ILightImplProvider impl) {
+		_handle = handle;
+		_impl = impl;
+	}
+
+	static Light IResource<Light>.RecreateFromRawHandleAndImpl(nuint rawHandle, IResourceImplProvider impl) {
+		return new Light(rawHandle, impl as ILightImplProvider ?? throw new InvalidOperationException($"Impl was '{impl}'."));
+	}
+
+	public void MoveBy(Vect translation) => Implementation.TranslateBy(_handle, translation);
+	public void RotateBy(Rotation rotation) => Implementation.RotateBy(_handle, rotation);
+
+	#region Disposal
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Dispose() => Implementation.Dispose(_handle);
+
+	internal bool IsDisposed {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => Implementation.IsDisposed(_handle);
+	}
+	#endregion
+
+	public override string ToString() => $"Light {(IsDisposed ? "(Disposed)" : $"\"{Name}\"")}";
+
+	#region Equality
+	public bool Equals(ModelInstance other) => _handle == other._handle && _impl.Equals(other._impl);
+	public override bool Equals(object? obj) => obj is ModelInstance other && Equals(other);
+	public override int GetHashCode() => HashCode.Combine((UIntPtr) _handle, _impl);
+	public static bool operator ==(ModelInstance left, ModelInstance right) => left.Equals(right);
+	public static bool operator !=(ModelInstance left, ModelInstance right) => !left.Equals(right);
+	#endregion
+}
