146bf10b14bb09e350938d4d9d7e0b86668a2a4b
Ben Bowen <Xenoprimate@users.noreply.github.com>
2025-03-29 20:46:44 +0000

    Pushing WIP documentation

--- Documentation/source/index.md
+++ Documentation/source/index.md
@@ -4,130 +4,43 @@ description: This is the homepage/manual for Tiny FFR (C# Tiny Fixed Function Re
 icon: fontawesome/solid/house
 ---
 
-# Welcome to MkDocs
+# Tiny Fixed Function Rendering in C\#
 
-| Method      | Description                          |
-| ----------: | :----------------------------------- |
-| `GET`       | :material-check:     Fetch resource  |
-| `PUT`       | :material-check-all: Update resource |
-| `DELETE`    | :material-close:     Delete resource |
+![Rotating Cube](tffrcube.webp){ align=right : style="height:200px;width:200px;border-radius:12px"}
 
-Tiny FFR blah (1) agadg 
-{ .annotate }
+TinyFFR (*Tiny* *F*ixed *F*unction *R*enderer) is a C# .NET9 library designed to help you render things in 3D:
 
-1.	test
+* Delivered via [NuGet](https://www.nuget.org/packages/Egodystonic.TinyFFR/)
+* Free for commercial and non-commercial use [(see license)](https://github.com/Egodystonic/TinyFFR/blob/main/LICENSE.md)
+* Physically-based rendering (via [filament](https://github.com/google/filament))
+* Asset loading (via [assimp](https://github.com/assimp/assimp) and [stb_image](https://github.com/nothings/stb))
+* Window management and input handling (via [SDL](https://github.com/libsdl-org/SDL))
+* Fully-abstracted math & geometry API - no pre-existing 3D or linear algebra knowledge required
+* Zero-GC design
 
-![Image title](https://dummyimage.com/600x400/eee/aaa)
-/// caption
-Caption here
-///
+# Who Should Use TinyFFR?
 
-!!! note blah
-	TYestse seog hsdfhog[^1] 
+* TinyFFR is designed for C#/.NET programmers that want to render 3D scenes or objects without 
+The "FFR" in TinyFFR stands for *F*ixed *F*unction *R*enderer. TinyFFR 
 
-## Commands
-
-* `mkdocs new [dir-name]` - Create a new project.
-* `mkdocs serve` - Start the live-reloading docs server.
-* `mkdocs build` - Build the documentation site.
-* `mkdocs -h` - Print help message and exit.
-
-## Project layout
-
-    mkdocs.yml    # The configuration file.
-    docs/
-        index.md  # The documentation homepage.
-        ...       # Other markdown pages, images and other files.
-
-=== "Tab 1"
-
-	```csharp
-	for (var a = 0f; a < 360f; a += 60f) {
-		var angle = new Angle(a); // (1)!
-		Console.WriteLine(PercentageUtils.ConvertFractionToPercentageString(angle.FullCircleFraction) + " = " + ColorVect.FromHueSaturationLightness(angle, 1f, 0.5f));
-	}
-	```
-
-	1. Test test test
-
-	```csharp
-	for (var a = 0f; a < 360f; a += 60f) {
-		var angle = new Angle(a);
-		Console.WriteLine(PercentageUtils.ConvertFractionToPercentageString(angle.FullCircleFraction) + " = " + ColorVect.FromHueSaturationLightness(angle, 1f, 0.5f));
-	}
-	```
-
-=== "Tab 2"
-
-	```csharp
-	for (var a = 0f; a < 360f; a += 60f) {
-		var angle = new Angle(a);
-		Console.WriteLine(PercentageUtils.ConvertFractionToPercentageString(angle.FullCircleFraction) + " = " + ColorVect.FromHueSaturationLightness(angle, 1f, 0.5f));
-	}
-	```
-	```csharp
-	for (var a = 0f; a < 360f; a += 60f) {
-		var angle = new Angle(a);
-		Console.WriteLine(PercentageUtils.ConvertFractionToPercentageString(angle.FullCircleFraction) + " = " + ColorVect.FromHueSaturationLightness(angle, 1f, 0.5f));
-	}
-	```
-	
-```csharp
-for (var a = 0f; a < 360f; a += 60f) {
-	var angle = new Angle(a);
-	Console.WriteLine(PercentageUtils.ConvertFractionToPercentageString(angle.FullCircleFraction) + " = " + ColorVect.FromHueSaturationLightness(angle, 1f, 0.5f));
-}
-```
+# Where to Start
 
 <div class="grid cards" markdown>
 
--   :material-clock-fast:{ .lg .middle } __Set up in 5 minutes__
+-   :material-clock-fast:{ .lg .middle } __Title__
 
     ---
 
-    Install [`mkdocs-material`](#) with [`pip`](#) and get up
-    and running in minutes
+    Summary here
 
-    [:octicons-arrow-right-24: Getting started](#)
+    [:octicons-arrow-right-24: Link](https://github.com/libsdl-org/SDL)
 
--   :fontawesome-brands-markdown:{ .lg .middle } __It's just Markdown__
-
-    ---
-
-    Focus on your content and generate a responsive and searchable static site
-
-    [:octicons-arrow-right-24: Reference](#)
-
--   :material-format-font:{ .lg .middle } __Made to measure__
+-   :material-clock-fast:{ .lg .middle } __Title__
 
     ---
 
-    Change the colors, fonts, language, icons, logo and more with a few lines
+    Summary here
 
-    [:octicons-arrow-right-24: Customization](#)
-
--   :material-scale-balance:{ .lg .middle } __Open Source, MIT__
-
-    ---
-
-    Material for MkDocs is licensed under MIT and available on [GitHub]
-
-    [:octicons-arrow-right-24: License](#)
+    [:octicons-arrow-right-24: Link](https://github.com/libsdl-org/SDL)
 
 </div>
-
-`Lorem ipsum dolor sit amet`
-
-:   Sed sagittis eleifend rutrum. Donec vitae suscipit est. Nullam tempus
-    tellus non sem sollicitudin, quis rutrum leo facilisis.
-
-`#!csharp var angle = new Angle(a);`
-
-:   Aliquam metus eros, pretium sed nulla venenatis, faucibus auctor ex. Proin
-    ut eros sed sapien ullamcorper consequat. Nunc ligula ante.
-
-    Duis mollis est eget nibh volutpat, fermentum aliquet dui mollis.
-    Nam vulputate tincidunt fringilla.
-    Nullam dignissim ultrices urna non auctor.
-
-[^1]: This is a footnote
--- LocalDevTesting/Program.cs
+++ LocalDevTesting/Program.cs
@@ -36,95 +36,103 @@ NativeLibrary.SetDllImportResolver( // Yeah this is ugly af but it'll do for v1
 	}
 );
 
-
-for (var a = 0f; a < 360f; a += 60f) {
-	var angle = new Angle(a);
-	Console.WriteLine(PercentageUtils.ConvertFractionToPercentageString(angle.FullCircleFraction) + " = " + ColorVect.FromHueSaturationLightness(angle, 1f, 0.5f));
-}
-
 using var factory = new LocalTinyFfrFactory();
 
-using var mesh = factory.AssetLoader.MeshBuilder.CreateMesh(new Cuboid(1f)); // 1m cube
-
-// using var colorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.Gradient(
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Right)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.UpRight)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Up)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.UpLeft)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Left)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.DownLeft)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Down)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.DownRight)!.Value, 1f, 0.5f),
-// 	ColorVect.White
-// ));
-using var colorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.ChequerboardBordered(
-	ColorVect.RandomOpaque(), 5, ColorVect.RandomOpaque()
+using var colorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.Gradient(
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Right)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.UpRight)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Up)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.UpLeft)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Left)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.DownLeft)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Down)!.Value, 1f, 0.5f),
+	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.DownRight)!.Value, 1f, 0.5f),
+	ColorVect.White
 ));
-// using var colorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.Lines(
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Right)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.UpRight)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Up)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.UpLeft)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Left)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.DownLeft)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.Down)!.Value, 1f, 0.5f),
-// 	ColorVect.FromHueSaturationLightness(Angle.From2DPolarAngle(Orientation2D.DownRight)!.Value, 1f, 0.5f),
-// 	horizontal: true,
-// 	perturbationMagnitude: 0.2f,
-// 	numRepeats: 1
+// using var colorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.ChequerboardBordered(
+// 	ColorVect.RandomOpaque(), 5, ColorVect.RandomOpaque()
 // ));
+// using var colorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.ChequerboardBordered(
+// 	ColorVect.RandomOpaque().WithLightness(0.5f),
+// 	16,
+// 	ColorVect.RandomOpaque().WithSaturation(0.85f),
+// 	ColorVect.RandomOpaque().WithSaturation(0.85f),
+// 	ColorVect.RandomOpaque().WithSaturation(0.85f),
+// 	ColorVect.RandomOpaque().WithSaturation(0.85f),
+// 	cellResolution: 256
+// ));
+using var normalMap = factory.AssetLoader.MaterialBuilder.CreateNormalMap(TexturePattern.Rectangles(
+	(256, 256),
+	(16, 16),
+	(0, 0),
+	Direction.Forward,
+	new Direction(-1f, 0f, 1f),
+	new Direction(0f, 1f, 1f),
+	new Direction(1f, 0f, 1f),
+	new Direction(0f, -1f, 1f),
+	Direction.Forward, 
+	(8, 8)
+));
+var ormPattern = TexturePattern.Rectangles<Real>(
+	(256, 256),
+	(16, 16),
+	(0, 0),
+	0f,
+	1f,
+	0.4f,
+	0f,
+	0.4f,
+	1f,
+	(8, 8)
+);
+using var ormMap = factory.AssetLoader.MaterialBuilder.CreateOrmMap(ormPattern, ormPattern, ormPattern);
 
-using var material = factory.AssetLoader.MaterialBuilder.CreateOpaqueMaterial(colorMap);
-var s = factory.ObjectBuilder.CreateModelInstance(mesh, material, initialPosition: new Location(0f, 0.05f, 1.6f) + Vect.Random(new(-0.1f), new(0.1f)));
-using var instance = factory.ObjectBuilder.CreateModelInstance(mesh, material);
+// Create a cuboid mesh and load an instance of it in to the world with a test material
+using var mesh = factory.AssetLoader.MeshBuilder.CreateMesh(new Cuboid(1f)); // 1m cube
+using var mat = factory.AssetLoader.MaterialBuilder.CreateOpaqueMaterial(
+	colorMap,
+	normalMap,
+	ormMap
+);
+using var instance = factory.ObjectBuilder.CreateModelInstance(
+  mesh,
+  mat
+);
 
-using var light = factory.LightBuilder.CreatePointLight(new Location(0f, 0f, 0f), new ColorVect(1f, 1f, 1f));
-using var rLight = factory.LightBuilder.CreatePointLight(new Location(1.6f, 2.6f, 1.6f), new ColorVect(0f, 1f, 0f));
-using var lLight = factory.LightBuilder.CreatePointLight(new Location(-1.6f, 2.6f, 1.6f), new ColorVect(1f, 0f, 0f));
-using var bLight = factory.LightBuilder.CreatePointLight(new Location(0f, 2.6f, 3.6f), new ColorVect(0f, 0f, 1f));
+// Create a light to illuminate the cube
+using var light = factory.LightBuilder.CreatePointLight(Location.Origin);
 
-using var window = factory.WindowBuilder.CreateWindow(factory.DisplayDiscoverer.Primary!.Value);
-using var scene = factory.SceneBuilder.CreateScene(includeBackdrop: true);
-scene.Add(s);
-s.SetPosition(instance.Position + Vect.Random());
+// Create a window to render to, 
+// a scene to render, 
+// a camera to capture the scene, 
+// and a renderer to render it all
+using var window = factory.WindowBuilder.CreateWindow(factory.DisplayDiscoverer.Primary!.Value, size: (800, 800));
+using var scene = factory.SceneBuilder.CreateScene();
 using var camera = factory.CameraBuilder.CreateCamera();
 using var renderer = factory.RendererBuilder.CreateRenderer(scene, camera, window);
 
-//scene.Add(instance);
+using var cubemap = factory.AssetLoader.LoadEnvironmentCubemap(@"C:\Users\ben\Documents\Egodystonic\TinyFFR\Repository\TinyFFR.Tests\IntegrationTests\kloofendal_48d_partly_cloudy_puresky_4k.hdr");
+scene.SetBackdrop(cubemap);
+
+// Add the cube instance and light to the scene
+scene.Add(instance);
 scene.Add(light);
-// scene.Add(rLight);
-// scene.Add(lLight);
-// scene.Add(bLight);
 
-instance.SetPosition(new Location(0f, 0.05f, 1.6f));
-instance.RotateBy(45f % Direction.Down);
-instance.RotateBy(45f % Direction.Right);
+// Put the cube 2m in front of the camera
+instance.SetPosition(new Location(0f, 0f, 2f));
 
-for (var i = 0; i < 6000; ++i) {
-	var q = factory.ObjectBuilder.CreateModelInstance(mesh, material, initialPosition: instance.Position + Vect.Random());
-	scene.Add(q);
-	q.SetPosition(instance.Position + Vect.Random());
-}
-
+// Keep rendering at 60Hz until the user closes the window
 using var loop = factory.ApplicationLoopBuilder.CreateLoop(60);
 while (!loop.Input.UserQuitRequested) {
 	var dt = (float) loop.IterateOnce().TotalSeconds;
 
-	if (loop.Input.KeyboardAndMouse.KeyWasPressedThisIteration(KeyboardOrMouseKey.Space)) {
-		var newColorMap = factory.AssetLoader.MaterialBuilder.CreateColorMap(TexturePattern.ChequerboardBordered(
-			ColorVect.RandomOpaque(), 3, 
-			ColorVect.RandomOpaque(),
-			ColorVect.RandomOpaque(),
-			ColorVect.RandomOpaque(),
-			ColorVect.RandomOpaque(),
-			repetitionCount: (4, 4)
-		));
-		var newMaterial = factory.AssetLoader.MaterialBuilder.CreateOpaqueMaterial(newColorMap);
-		instance.Material = newMaterial;
+	// If we're holding space down, rotate the cube
+	if (loop.Input.KeyboardAndMouse.KeyIsCurrentlyDown(KeyboardOrMouseKey.Space)) {
+		camera.RotateBy(new Rotation(angle: 90f, axis: Direction.Down) * dt);
+		camera.Position = instance.Position - camera.ViewDirection * 2f;
+		light.Position = camera.Position + (Direction.Up * MathF.Sin(3f * (float) loop.TotalIteratedTime.TotalSeconds));
+		//instance.RotateBy(new Rotation(angle: 90f, axis: Direction.Down) * dt);
 	}
-
-	camera.RotateBy((90f % Direction.Down) * dt);
-	camera.RotateBy((90f % Direction.Right) * dt);
 
 	renderer.Render();
 }
