83f8f2ba8be48afb810355edbbc837708b55cab1
Ben Bowen <Xenoprimate@users.noreply.github.com>
2025-05-27 11:14:03 +0100

    v0.2 changes

---
+++ NupkgTesting/AssetTestConstants.cs
@@ -0,0 +1,40 @@
+﻿// Created on 2025-05-27 by Ben Bowen
+// (c) Egodystonic / TinyFFR 2025
+
+using Egodystonic.TinyFFR.Assets.Materials;
+
+namespace NupkgTesting;
+
+public static class AssetTestConstants {
+	public const string AlbedoFile = "ELCrate.png";
+	public const string NormalFile = "ELCrate_Normal.png";
+	public const string SpecularFile = "ELCrate_Specular.png";
+	public const string MeshFile = "ELCrate.obj";
+	public const string SkyboxFile = "kloofendal_48d_partly_cloudy_puresky_4k.hdr";
+	public const string LogoFile = "egdLogo.png";
+	// These values were sampled/taken from an external paint program
+	public static readonly Dictionary<int, TexelRgb24> _expectedSampledAlbedoPixelValues = new() {
+		[1024 * 0000 + 0000] = new(0, 0, 0),
+		[1024 * 1023 + 0000] = new(66, 56, 41),
+		[1024 * 1023 + 1023] = new(122, 110, 89),
+		[1024 * 0000 + 1023] = new(0, 0, 0),
+	};
+	public static readonly Dictionary<int, TexelRgb24> _expectedSampledNormalPixelValues = new() {
+		[1024 * 0180 + 0374] = new(127, 127, 255),
+		[1024 * 0345 + 0786] = new(127, 228, 205),
+		[1024 * 0412 + 1001] = new(133, 126, 255),
+		[1024 * 0497 + 0284] = new(245, 123, 176),
+	};
+	public static readonly Dictionary<int, TexelRgb24> _expectedSampledSpecularPixelValues = new() {
+		[1024 * 0187 + 0344] = new(237, 237, 237),
+		[1024 * 0270 + 0360] = new(23, 23, 23),
+		[1024 * 0803 + 0373] = new(159, 159, 159),
+		[1024 * 1023 + 1023] = new(0, 0, 0),
+	};
+
+	public static void AssertTexelSamples(Span<TexelRgb24> readData, Dictionary<int, TexelRgb24> expectation) {
+		foreach (var kvp in expectation) {
+			if (kvp.Value != readData[kvp.Key]) throw new InvalidOperationException($"Failed at texel {kvp.Key}.");
+		}
+	}
+}
--- NupkgTesting/Program.cs
+++ NupkgTesting/Program.cs
@@ -1,44 +1,67 @@
 ﻿using Egodystonic.TinyFFR;
+using Egodystonic.TinyFFR.Assets;
 using Egodystonic.TinyFFR.Assets.Materials;
-using Egodystonic.TinyFFR.Factory.Local;
+using Egodystonic.TinyFFR.Assets.Meshes;
 using Egodystonic.TinyFFR.Environment.Input;
+using Egodystonic.TinyFFR.Factory.Local;
+using static NupkgTesting.AssetTestConstants;
 
 using var factory = new LocalTinyFfrFactory();
-using var cubeMesh = factory.AssetLoader.MeshBuilder.CreateMesh(new Cuboid(1f));
-var materialBuilder = factory.AssetLoader.MaterialBuilder;
 
-using var colorMap = materialBuilder.CreateColorMap(ColorVect.White.WithLightness(0.2f));
-using var normalMap = materialBuilder.CreateNormalMap(TexturePattern.Rectangles(
-	interiorSize: (64, 64),
-	borderSize: (8, 8),
-	paddingSize: (32, 32),
-	interiorValue: new Direction(0f, 0f, 1f),
-	borderRightValue: new Direction(-1f, 0f, 1f),
-	borderTopValue: new Direction(0f, 1f, 1f),
-	borderLeftValue: new Direction(1f, 0f, 1f),
-	borderBottomValue: new Direction(0f, -1f, 1f),
-	paddingValue: new Direction(0f, 0f, 1f),
-	repetitions: (6, 6)
-));
+if (new TextureReadMetadata(1024, 1024) != factory.AssetLoader.ReadTextureMetadata(AlbedoFile)) throw new InvalidOperationException("Test fail");
+if (new TextureReadMetadata(1024, 1024) != factory.AssetLoader.ReadTextureMetadata(NormalFile)) throw new InvalidOperationException("Test fail");
+if (new TextureReadMetadata(1024, 1024) != factory.AssetLoader.ReadTextureMetadata(SpecularFile)) throw new InvalidOperationException("Test fail");
 
-using var material = factory.AssetLoader.MaterialBuilder.CreateOpaqueMaterial(colorMap, normalMap);
-using var cube = factory.ObjectBuilder.CreateModelInstance(cubeMesh, material, initialPosition: (0f, 0f, 2f));
-using var light = factory.LightBuilder.CreatePointLight(Location.Origin);
+var texBuffer = (new TexelRgb24[1024 * 1024]).AsSpan();
+factory.AssetLoader.ReadTexture(AlbedoFile, texBuffer);
+AssertTexelSamples(texBuffer, _expectedSampledAlbedoPixelValues);
+factory.AssetLoader.ReadTexture(NormalFile, texBuffer);
+AssertTexelSamples(texBuffer, _expectedSampledNormalPixelValues);
+factory.AssetLoader.ReadTexture(SpecularFile, texBuffer);
+AssertTexelSamples(texBuffer, _expectedSampledSpecularPixelValues);
+
+if (new MeshReadMetadata(662, 480) != factory.AssetLoader.ReadMeshMetadata(MeshFile)) throw new InvalidOperationException("Test fail");
+var meshVertexBuffer = new MeshVertex[662];
+var meshTriangleBuffer = new VertexTriangle[480];
+factory.AssetLoader.ReadMesh(MeshFile, meshVertexBuffer, meshTriangleBuffer);
+var minLoc = new Location(Single.MaxValue, Single.MaxValue, Single.MaxValue);
+var maxLoc = new Location(Single.MinValue, Single.MinValue, Single.MinValue);
+foreach (var v in meshVertexBuffer) {
+	var loc = v.Location;
+	minLoc = new(Single.Min(minLoc.X, loc.X), Single.Min(minLoc.Y, loc.Y), Single.Min(minLoc.Z, loc.Z));
+	maxLoc = new(Single.Max(maxLoc.X, loc.X), Single.Max(maxLoc.Y, loc.Y), Single.Max(maxLoc.Z, loc.Z));
+}
+var calculatedOrigin = ((minLoc.AsVect() + maxLoc.AsVect()) * 0.5f).AsLocation();
+if (new Location(0f, 11.344924f, 1.9073486E-06f) != calculatedOrigin) throw new InvalidOperationException("Test fail");
+
+var display = factory.DisplayDiscoverer.Recommended!.Value;
+using var window = factory.WindowBuilder.CreateWindow(display, title: "Local Asset Import Test");
+window.SetIcon(LogoFile);
+using var camera = factory.CameraBuilder.CreateCamera(Location.Origin);
+using var albedo = factory.AssetLoader.LoadTexture(AlbedoFile);
+using var normal = factory.AssetLoader.LoadTexture(NormalFile);
+using var orm = factory.AssetLoader.LoadAndCombineOrmTextures(roughnessMapFilePath: SpecularFile, metallicMapFilePath: SpecularFile, config: new() { InvertYGreenChannel = true });
+using var mat = factory.AssetLoader.MaterialBuilder.CreateOpaqueMaterial(albedo, normal, orm);
+using var mesh = factory.AssetLoader.LoadMesh(MeshFile, new MeshCreationConfig { LinearRescalingFactor = 0.03f, OriginTranslation = calculatedOrigin.AsVect() });
+using var instance = factory.ObjectBuilder.CreateModelInstance(mesh, mat, initialPosition: camera.Position + Direction.Forward * 1.3f);
+using var cubemap = factory.AssetLoader.LoadEnvironmentCubemap(SkyboxFile);
 using var scene = factory.SceneBuilder.CreateScene();
-
-scene.Add(cube);
-scene.Add(light);
-
-using var window = factory.WindowBuilder.CreateWindow(factory.DisplayDiscoverer.Primary!.Value);
-using var camera = factory.CameraBuilder.CreateCamera(initialPosition: Location.Origin, initialViewDirection: Direction.Forward);
-
 using var renderer = factory.RendererBuilder.CreateRenderer(scene, camera, window);
-using var loop = factory.ApplicationLoopBuilder.CreateLoop(60);
-var input = loop.Input;
-var kbm = input.KeyboardAndMouse;
 
-while (!input.UserQuitRequested) {
-	var deltaTime = (float) loop.IterateOnce().TotalSeconds;
-	if (kbm.KeyIsCurrentlyDown(KeyboardOrMouseKey.Space)) cube.RotateBy(90f % Direction.Down * deltaTime);
+scene.Add(instance);
+scene.SetBackdrop(cubemap, 0f);
+
+var instanceToCameraVect = instance.Position >> camera.Position;
+
+using var loop = factory.ApplicationLoopBuilder.CreateLoop(60);
+while (!loop.Input.UserQuitRequested && loop.TotalIteratedTime < TimeSpan.FromSeconds(8.9d)) {
+	_ = loop.IterateOnce();
 	renderer.Render();
+
+	instanceToCameraVect = instanceToCameraVect.RotatedBy(2.3f % Direction.Up);
+	camera.Position = instance.Position + instanceToCameraVect + (Direction.Up * MathF.Sin((float) loop.TotalIteratedTime.TotalSeconds * 1.67f));
+	camera.ViewDirection = (camera.Position >> instance.Position).Direction;
+	var cbBrightness = (float) Math.Floor(loop.TotalIteratedTime.TotalSeconds) * 0.25f;
+	scene.SetBackdrop(cubemap, cbBrightness);
+	window.SetTitle("Backdrop brightness level " + PercentageUtils.ConvertFractionToPercentageString(cbBrightness));
 }
