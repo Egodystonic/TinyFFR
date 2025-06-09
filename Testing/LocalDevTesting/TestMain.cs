cc983003f42038c08470be37cee68d65e1b5b9a2
Ben Bowen <Xenoprimate@users.noreply.github.com>
2025-06-09 02:06:41 +0100

    Add git instructions to testmain.cs

--- Testing/LocalDevTesting/TestMain.cs
+++ Testing/LocalDevTesting/TestMain.cs
@@ -14,7 +14,14 @@ namespace Egodystonic.TinyFFR.Testing.Local;
 //	Tick() will then be called every frame, unless you set options.UseDefaultLoop to false (in which case you can call it yourself manually or ignore it).
 //	There are some default objects accessible via properties (see TestMain.Scaffold.cs).
 //	Call ExitTest() to finish the test.
-//	This file is ignored in git, so you can edit it freely without creating merge conflicts
+
+// Anti-merge-issues:
+//	Because this is a test ground for each developer, ideally before editing this file
+//	you'd execute the following command in git to never push any changes up:
+//
+//	git update-index --assume-unchanged Testing/LocalDevTesting/TestMain.cs
+//	
+//	After that you can modify this file as you wish.
 
 static partial class TestMain {
 	public static void ConfigureTest(TestOptions options) {
