c3beccabe1ac820d01ec0ba6762023aefee17ccc
Ben Bowen <Xenoprimate@users.noreply.github.com>
2025-06-14 19:26:39 +0100

    More work on build/copy script, committing now before running it once in case it's busted

--- ThirdParty/build_and_copy_all.cs
+++ ThirdParty/build_and_copy_all.cs
@@ -10,7 +10,12 @@ using System.Linq;
 
 const string ScriptDirName = "ThirdParty";
 const string BuildOutputDirName = "build_output";
+const string InterimInstallDirName = "install";
 const string ThirdPartyNativeDirPath = "TinyFFR.Native/third_party";
+const string ThirdPartyNativeBinariesDirName = "binaries";
+const string ThirdPartyNativeHeadersDirName = "headers";
+var interimInstallBinaryFolderNames = new[] { "bin", "lib" };
+var interimInstallHeaderFolderNames = new[] { "include" };
 
 const string LibAssimp = "assimp";
 const string LibFilament = "filament";
@@ -21,26 +26,35 @@ const string BuildOutputDirToken = "%BUILDOUTDIR%";
 const string ConfigurationToken = "%CONFIG%";
 var cmakeInvocationsDict = new Dictionary<string, List<string>> {
 	[LibAssimp] = new() {
-		$"-DCMAKE_INSTALL_PREFIX=install \"{RepoRootDirToken}/CMakeLists.txt\"",
+		$"-DCMAKE_INSTALL_PREFIX={InterimInstallDirName} -DASSIMP_INSTALL=OFF -DASSIMP_BUILD_TESTS=OFF \"{RepoRootDirToken}/CMakeLists.txt\"",
 		$"--build . --config {ConfigurationToken}",
 		$"--build . --target install --config {ConfigurationToken}",
 	},
 	[LibFilament] = new() {
-		$"\"{RepoRootDirToken}/CMakeLists.txt\"",
+		$"-DCMAKE_INSTALL_PREFIX={InterimInstallDirName} \"{RepoRootDirToken}/CMakeLists.txt\"",
 		$"--build . --config {ConfigurationToken}",
 		$"--build . --target install --config {ConfigurationToken}",
 	},
 	[LibSdl] = new() {
-		$"-DCMAKE_INSTALL_PREFIX=install \"{RepoRootDirToken}/CMakeLists.txt\"",
+		$"-DCMAKE_INSTALL_PREFIX={InterimInstallDirName} \"{RepoRootDirToken}/CMakeLists.txt\"",
 		$"--build . --config {ConfigurationToken}",
 		$"--build . --target install --config {ConfigurationToken}",
 	}
 };
 
+const string DebugConfiguration = "Debug";
+const string ReleaseConfiguration = "Release";
+var configurations = new[] { DebugConfiguration, ReleaseConfiguration };
+
+const string BuildStep = "build";
+const string CopyStep = "copy";
+var stepNames = new[] { BuildStep, CopyStep };
+
 // ===================================================================================================================================
 // Script starts here
 // ===================================================================================================================================
 
+// 1 -- Set up directories
 Console.WriteLine("Starting Third Party Build & Copy");
 
 var scriptDir = Directory.GetCurrentDirectory();
@@ -68,39 +82,103 @@ if (!Directory.Exists(thirdPartyNativeDir)) {
 	_ = Directory.CreateDirectory(thirdPartyNativeDir);
 }
 
+string GetBuildOutputPath(string lib, string configuration) {
+	return Path.Combine(interimBuildOutputDir, lib, configuration);
+}
+string GetThirdPartyRepoRootPath(string lib) {
+	return Path.Combine(scriptDir, lib);
+}
+string GetInterimInstallDirPath(string lib, string configuration) {
+	return Path.Combine(interimBuildOutputDir, lib, configuration, InterimInstallDirName);
+}
+string GetDestinationBinariesPath(string lib, string configuration) {
+	return Path.Combine(thirdPartyNativeDir, ThirdPartyNativeBinariesDirName, lib.ToLowerInvariant(), configuration.ToLowerInvariant());
+}
+string GetDestinationHeadersPath(string lib) {
+	return Path.Combine(thirdPartyNativeDir, ThirdPartyNativeHeadersDirName, lib.ToLowerInvariant());
+}
+
+
+// 2 -- Parse command line args
+var commandLineLibArg = cmakeInvocationsDict.Keys.FirstOrDefault(k => Environment.GetCommandLineArgs().Any(a => a.Equals(k, StringComparison.OrdinalIgnoreCase)));
+var commandLineConfigArg = configurations.FirstOrDefault(c => Environment.GetCommandLineArgs().Any(a => a.Equals(c, StringComparison.OrdinalIgnoreCase)));
+var commandLineStepArg = stepNames.FirstOrDefault(s => Environment.GetCommandLineArgs().Any(a => a.Equals(s, StringComparison.OrdinalIgnoreCase)));
+
 Console.WriteLine($"Script Dir: {scriptDir}");
 Console.WriteLine($"Interim Build Output Dir: {interimBuildOutputDir}");
 Console.WriteLine($"Third Party Native Dir: {thirdPartyNativeDir}");
-
-string GetBuildOutputDirForLib(string lib, string configuration) {
-	return Path.Combine(interimBuildOutputDir, lib, configuration);
+if (commandLineLibArg == null) {
+	Console.WriteLine("Executing for all third-party dependencies.");
 }
-string GetThirdPartyRepoRootDirForLib(string lib) {
-	return Path.Combine(scriptDir, lib);
+else {
+	Console.WriteLine($"Executing for dependency '{commandLineLibArg}' only.");
+}
+if (commandLineConfigArg == null) {
+	Console.WriteLine("Executing for all configurations.");
+}
+else {
+	Console.WriteLine($"Executing for configuration '{commandLineConfigArg}' only.");
+}
+if (commandLineStepArg == null) {
+	Console.WriteLine("Executing all steps.");
+}
+else {
+	Console.WriteLine($"Executing step '{commandLineStepArg}' only.");
 }
 
-void InvokeCmakeForLib(string lib, string configuration) {
+// 3 -- Execute according to commandline
+if (commandLineStepArg == null || commandLineStepArg == BuildStep) {
+	foreach (var configuration in configurations) {
+		if (commandLineConfigArg != null && configuration != commandLineConfigArg) continue;
+
+		if (commandLineLibArg != null) {
+			BuildLib(commandLineLibArg, configuration);
+		}
+		else {
+			BuildLib(LibAssimp, configuration);
+			BuildLib(LibFilament, configuration);
+			BuildLib(LibSdl, configuration);
+		}
+	}
+}
+if (commandLineStepArg == null || commandLineStepArg == CopyStep) {
+	foreach (var configuration in configurations) {
+		if (commandLineConfigArg != null && configuration != commandLineConfigArg) continue;
+
+		if (commandLineLibArg != null) {
+			CopyLib(commandLineLibArg, configuration);
+		}
+		else {
+			CopyLib(LibAssimp, configuration);
+			CopyLib(LibFilament, configuration);
+			CopyLib(LibSdl, configuration);
+		}
+	}
+}
+
+// 4 -- Local functions for each step
+void BuildLib(string lib, string configuration) {
 	try {
 		Console.WriteLine();
 		Console.WriteLine($"===============================================");
-		Console.WriteLine($"{lib.ToUpperInvariant()} | {configuration}");
+		Console.WriteLine($"BUILD | {lib.ToUpperInvariant()} | {configuration}");
 		Console.WriteLine($"===============================================");
 		Console.WriteLine($"Beginning cmake for {lib}...");
-		var buildOutputDir = GetBuildOutputDirForLib(lib, configuration);
-		var thirdPartyRepoRootDir = GetThirdPartyRepoRootDirForLib(lib);
+		var buildOutputDir = GetBuildOutputPath(lib, configuration);
+		var thirdPartyRepoRootDir = GetThirdPartyRepoRootPath(lib);
 		if (Directory.Exists(buildOutputDir)) Directory.Delete(buildOutputDir, true);
 		Directory.CreateDirectory(buildOutputDir);
 		Directory.SetCurrentDirectory(buildOutputDir);
 		Console.WriteLine($"Build output directory: {buildOutputDir}");
 		Console.WriteLine($"Repository root directory: {thirdPartyRepoRootDir}");
 		Console.WriteLine();
-		
+
 		foreach (var invocation in cmakeInvocationsDict[lib]) {
 			var parsedInvocation = invocation
 				.Replace(RepoRootDirToken, thirdPartyRepoRootDir)
 				.Replace(BuildOutputDirToken, buildOutputDir)
 				.Replace(ConfigurationToken, configuration);
-			
+
 			Console.WriteLine();
 			Console.WriteLine($"> cmake {parsedInvocation}");
 			var process = Process.Start("cmake", parsedInvocation);
@@ -109,7 +187,7 @@ void InvokeCmakeForLib(string lib, string configuration) {
 			Console.WriteLine();
 		}
 
-		Console.WriteLine($"Finished cmake for {lib}.");
+		Console.WriteLine($"Finished cmake for {lib}:{configuration}.");
 		Console.WriteLine();
 		Console.WriteLine();
 	}
@@ -118,15 +196,61 @@ void InvokeCmakeForLib(string lib, string configuration) {
 	}
 }
 
-var commandLineLibArg = cmakeInvocationsDict.Keys.FirstOrDefault(k => Environment.GetCommandLineArgs().Any(a => a.Equals(k, StringComparison.OrdinalIgnoreCase)));
+void CopyLib(string lib, string configuration) {
+	Console.WriteLine();
+	Console.WriteLine($"===============================================");
+	Console.WriteLine($"COPY | {lib.ToUpperInvariant()} | {configuration}");
+	Console.WriteLine($"===============================================");
+	var interimInstallDir = GetInterimInstallDirPath(lib, configuration);
+	if (commandLineConfigArg != null || configuration == ReleaseConfiguration) {
+		var destinationHeadersDir = GetDestinationHeadersPath(lib);
+		Console.WriteLine($"Clearing contents from '{destinationHeadersDir}'...");
+		if (Directory.Exists(destinationHeadersDir)) Directory.Delete(destinationHeadersDir, true);
+		Directory.CreateDirectory(destinationHeadersDir);
+		Console.WriteLine($"Searching for headers...");
+		foreach (var h in interimInstallHeaderFolderNames) {
+			var interimHeaderDir = Path.Combine(interimInstallDir, h);
+			if (!Directory.Exists(interimHeaderDir)) {
+				Console.WriteLine($"\tNone found at '{interimHeaderDir}'.");
+				continue;
+			}
 
-foreach (var configuration in new[] { "Debug", "Release" }) {
-	if (commandLineLibArg != null) {
-		InvokeCmakeForLib(commandLineLibArg, configuration);
+			Console.WriteLine($"\tCopying everything from '{interimHeaderDir}'...");
+			CopyDirectory(interimHeaderDir, interimHeaderDir, destinationHeadersDir);
+		}
 	}
-	else {
-		InvokeCmakeForLib(LibAssimp, configuration);
-		InvokeCmakeForLib(LibFilament, configuration);
-		InvokeCmakeForLib(LibSdl, configuration);
+
+	var destinationBinariesDir = GetDestinationBinariesPath(lib, configuration);
+	Console.WriteLine($"Clearing contents from '{destinationBinariesDir}'...");
+	if (Directory.Exists(destinationBinariesDir)) Directory.Delete(destinationBinariesDir, true);
+	Directory.CreateDirectory(destinationBinariesDir);
+	Console.WriteLine($"Searching for binaries...");
+	foreach (var b in interimInstallBinaryFolderNames) {
+		var interimBinaryDir = Path.Combine(interimInstallDir, b);
+		if (!Directory.Exists(interimBinaryDir)) {
+			Console.WriteLine($"\tNone found at '{interimBinaryDir}'.");
+			continue;
+		}
+
+		Console.WriteLine($"\tCopying everything from '{interimBinaryDir}'...");
+		CopyDirectory(interimBinaryDir, interimBinaryDir, destinationBinariesDir);
+	}
+
+	Console.WriteLine($"Finished copy for {lib}:{configuration}.");
+	Console.WriteLine();
+	Console.WriteLine();
+}
+
+static void CopyDirectory(string sourceDirOriginal, string sourceDir, string destinationDir) {
+	var sourceDirInfo = new DirectoryInfo(sourceDir);
+	Directory.CreateDirectory(destinationDir);
+
+	foreach (var file in sourceDirInfo.GetFiles()) {
+		file.CopyTo(Path.Combine(destinationDir, file.Name));
+		Console.WriteLine(file.FullName[sourceDirOriginal.Length..]);
+	}
+
+	foreach (var subDir in sourceDirInfo.GetDirectories()) {
+		CopyDirectory(sourceDirOriginal, subDir.FullName, Path.Combine(destinationDir, subDir.Name));
 	}
 }
