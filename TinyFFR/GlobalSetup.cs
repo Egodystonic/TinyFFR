d3b77b26a1b134184a81f27f77b1909d076cf034
Ben Bowen <Xenoprimate@users.noreply.github.com>
2025-10-03 19:21:12 +0100

    Revisited old csproj inclusions that were no longer necessary

--- TinyFFR/GlobalSetup.cs
+++ TinyFFR/GlobalSetup.cs
@@ -1,17 +1,6 @@
 ﻿// Created on 2024-01-11 by Ben Bowen
 // (c) Egodystonic / TinyFFR 2024
 
-using System.Reflection;
-using Egodystonic.TinyFFR;
-
 [assembly: InternalsVisibleTo("TinyFFR.Tests")]
 [assembly: InternalsVisibleTo("TinyFFR.Integrations.Common")]
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-[assembly: AssemblyVersion(TffrAssemblyMetadata.TffrVersion + ".0")]
-[assembly: AssemblyFileVersion(TffrAssemblyMetadata.TffrVersion)]
-[assembly: AssemblyInformationalVersion(TffrAssemblyMetadata.TffrVersion)]
-
-namespace Egodystonic.TinyFFR;
-static class TffrAssemblyMetadata {
-	public const string TffrVersion = "0.4.0";
-}
