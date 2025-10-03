07f15c78d9a8f6f74a1084aade5848b88ef1074c
Ben Bowen <Xenoprimate@users.noreply.github.com>
2025-10-03 19:13:00 +0100

    Added assembly version info to main dll

--- TinyFFR/GlobalSetup.cs
+++ TinyFFR/GlobalSetup.cs
@@ -2,7 +2,16 @@
 // (c) Egodystonic / TinyFFR 2024
 
 using System.Reflection;
+using Egodystonic.TinyFFR;
 
 [assembly: InternalsVisibleTo("TinyFFR.Tests")]
 [assembly: InternalsVisibleTo("TinyFFR.Integrations.Common")]
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+[assembly: AssemblyVersion(TffrAssemblyMetadata.TffrVersion + ".0")]
+[assembly: AssemblyFileVersion(TffrAssemblyMetadata.TffrVersion)]
+[assembly: AssemblyInformationalVersion(TffrAssemblyMetadata.TffrVersion)]
+
+namespace Egodystonic.TinyFFR;
+static class TffrAssemblyMetadata {
+	public const string TffrVersion = "0.4.0";
+}
