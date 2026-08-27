f6049549c777d512203ad4b64d261f07808a02af
Ben Bowen <Xenoprimate@users.noreply.github.com>
2026-08-27 21:23:02 +0100

    Cleaned up test proj

--- Testing/LocalDevTesting/TestMain.cs
+++ Testing/LocalDevTesting/TestMain.cs
@@ -40,8 +40,7 @@ static partial class TestMain {
 		//			Some values depend on others; for example if you set "builder.Context.Factory = null;" no other resources will be created by default.
 		//		You can use context properties to create others.
 		//			For example: "builder.Context.Loop = builder.Context.Factory!.ApplicationLoopBuilder.CreateLoop();" is completely fine.
-		builder.Context.Factory = new LocalTinyFfrFactory(assetLoaderConfig: new LocalAssetLoaderConfig() { MaxCachedTextMeshesPerFont = 64 }, rendererBuilderConfig: new RendererBuilderConfig { EnableVSync = false });
-		builder.DefaultLoopSlowFrameReportingEnable = false;
+
 	}
 
 	public static void StartTest(TestContext context) {
@@ -51,55 +50,11 @@ static partial class TestMain {
 		//		You can remove BeginDefaultLoop and Tick if you prefer to write your own loop.
 		//		The Tick function passed to BeginDefaultLoop should return `true` to exit the loop.
 		//		If you pass a CameraController to BeginDefaultLoop, it will be possible to control the camera with keyboard/mouse or gamepad using the default controller input mapping.
-
-		static string RandomStr() => Guid.NewGuid().ToString();
-		
-		var startTime = Stopwatch.GetTimestamp();
-		var font = context.Factory.AssetLoader.LoadFont(BuiltInFont.Default);
-		Console.WriteLine(Stopwatch.GetElapsedTime(startTime));
-		var pen = font.CreatePen(BuiltInFontPenStyle.WhiteWithOutline);
-		const int NumStrings = 30;
-		
-		List<string> storedStrings = new List<string>();
-		List<CameraLockedTextInstance> texts = new();
-		for (var i = 0; i < NumStrings; ++i) {
-			var s = RandomStr();
-			storedStrings.Add(s);
-			texts.Add(context.Factory.ObjectBuilder.CreateCameraLockedTextInstance(pen, font.CreateString(s), Location.Random(Sphere.UnitSphere)));
-			context.Scene.Add(texts[i]);
-		}
-		
-		var fontOp = context.Factory.AssetLoader.LoadFontAsync(BuiltInFont.Monospace, "myfontname");
-		var skyboxOp = context.Factory.AssetLoader.LoadBackdropTextureAsync(CommonTestAssets.FindAsset(KnownTestAsset.CloudsHdr), BackdropTextureResolution.VeryHigh);
 		
 		BeginDefaultLoop(Tick, context.Loop, context.CameraController);
 		bool Tick(float deltaTime) {
 			// Write anything you like here to be executed once per frame.
 			
-			for (var i = 0; i < NumStrings; ++i) {
-				var prevStr = texts[i].String;
-				var newStr = font.CreateString(RandomStr());
-				texts[i].SetString(newStr);
-				texts[i].SetPen(pen);
-				prevStr.Dispose();
-			}
-			
-			if (skyboxOp.IsResultAvailable) {
-				context.Scene.SetBackdrop(skyboxOp.GetResultAndDisposeOperation());
-			}
-			if (fontOp.IsResultAvailable) {
-				font = fontOp.GetResultAndDisposeOperation();
-				pen = font.CreatePen(BuiltInFontPenStyle.WhiteWithOutline);
-			}
-			// if (fontOp != null) {
-			// 	Console.WriteLine(TinyFfrAsyncOperation.GetCompletionStats(fontOp.Value, fontOp2, fontOp3, fontOp4, fontOp5, fontOp6));
-			// }
-			// if (fontOp?.IsCompleted ?? false) {
-			// 	font = fontOp.Value.GetResultAndDisposeOperation();
-			// 	fontOp = null;
-			// 	Console.WriteLine(Stopwatch.GetElapsedTime(startTime));
-			// }
-		
 			context.Renderer.Render();
 			return context.Input.KeyboardAndMouse.KeyWasPressedThisIteration(KeyboardOrMouseKey.Escape);
 		}
